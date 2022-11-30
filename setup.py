d93cd7cfa34a570fd51a8745a5df464096b55960
Vic Mochengo <victormochengo@gmail.com>
2022-11-30 17:37:43 +0300

    Added cookiecutter project template for Kaggle Data Science Survey 2022 data story

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploratory analysis of Kaggle Data Science survey of 2022',
+    author='victor mochengo',
+    license='MIT',
+)
